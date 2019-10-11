66dec106edd43ddf05169f0e476ba22eb339428f
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2019-10-11 18:20:03 +0200

    chore: update dependencies

--- @citd/client/webpack.config.js
+++ @citd/client/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       title: 'SFEIR Code in the Dark'
