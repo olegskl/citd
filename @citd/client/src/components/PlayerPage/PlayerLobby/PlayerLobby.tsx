0ba2751ad68ec2e17beb60176a49af269aec5459
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2020-08-15 19:29:32 +0300

    refactor(client): move PlayerLobby to FC

--- @citd/client/src/components/PlayerPage/PlayerLobby/PlayerLobby.tsx
+++ @citd/client/src/components/PlayerPage/PlayerLobby/PlayerLobby.tsx
@@ -8,54 +8,52 @@ import './PlayerLobby.css';
 
 type LobbyProps = ISocketContext & UserContext & GameContext;
 
-class PlayerLobbyComponent extends React.PureComponent<LobbyProps> {
-  private onJoinGame = () => {
-    this.props.socket.emit('joinGame', this.props.user.id);
+const PlayerLobbyComponent: React.FC<LobbyProps> = ({ socket, user, game }) => {
+  const onJoinGame = () => {
+    socket.emit('joinGame', user.id);
   };
 
-  private onLeaveGame = () => {
-    this.props.socket.emit('leaveGame', this.props.user.id);
+  // const onLeaveGame = () => {
+  //   socket.emit('leaveGame', user.id);
+  // };
+
+  const onReady = () => {
+    socket.emit('readyToPlay', user.id, true);
   };
 
-  private onReady = () => {
-    this.props.socket.emit('readyToPlay', this.props.user.id, true);
+  const onNotReady = () => {
+    socket.emit('readyToPlay', user.id, false);
   };
 
-  private onNotReady = () => {
-    this.props.socket.emit('readyToPlay', this.props.user.id, false);
-  };
+  const userInGame = game.players.find(({ id }) => id === user.id);
+  const isReadyToPlay = userInGame && userInGame.readyToPlay;
 
-  render() {
-    const { user, game } = this.props;
-    const userInGame = game.players.find(({ id }) => id === user.id);
-    const isReadyToPlay = userInGame && userInGame.readyToPlay;
-    return (
-      <div className="player-lobby">
-        <h1 className="text-glitchy-large">Code in the Dark</h1>
-        {userInGame ? (
-          <>
-            <div className="text-glitchy-medium">Are you ready?</div>
-            {isReadyToPlay ? (
-              <button className="button-glitchy-yellow" onClick={this.onNotReady}>
-                I&apos;m not ready
-              </button>
-            ) : (
-              <button className="button-glitchy-yellow" onClick={this.onReady}>
-                I&apos;m ready
-              </button>
-            )}
-          </>
-        ) : (
-          <>
-            <div className="text-glitchy-medium">Try to join the game...</div>
-            <button className="button-glitchy-yellow" onClick={this.onJoinGame}>
-              Join the game
+  return (
+    <div className="player-lobby">
+      <h1 className="text-glitchy-large">Code in the Dark</h1>
+      {userInGame ? (
+        <>
+          <div className="text-glitchy-medium">Are you ready?</div>
+          {isReadyToPlay ? (
+            <button className="button-glitchy-yellow" onClick={onNotReady}>
+              I&apos;m not ready
             </button>
-          </>
-        )}
-      </div>
-    );
-  }
-}
+          ) : (
+            <button className="button-glitchy-yellow" onClick={onReady}>
+              I&apos;m ready
+            </button>
+          )}
+        </>
+      ) : (
+        <>
+          <div className="text-glitchy-medium">Try to join the game...</div>
+          <button className="button-glitchy-yellow" onClick={onJoinGame}>
+            Join the game
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
 
-export const PlayerLobby = withSocket(withUser(withGame(PlayerLobbyComponent)));
+export const PlayerLobby = withSocket(withUser(withGame(React.memo(PlayerLobbyComponent))));
