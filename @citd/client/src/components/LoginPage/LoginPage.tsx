8fd8cb24f6471ec7f65e8e87c9b43dccd35af91a
Artem Sklyanchuk <artem.sklyancuk@gmail.com>
2020-08-15 14:29:24 +0300

    refactor(client): move LoginPage to hooks

--- @citd/client/src/components/LoginPage/LoginPage.tsx
+++ @citd/client/src/components/LoginPage/LoginPage.tsx
@@ -1,64 +1,61 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { ISocketContext, withSocket } from '../../context/socket';
+import { ISocketContext, withSocket } from "../../context/socket";
 
-import './LoginPage.css';
+import "./LoginPage.css";
 
-interface LoginPageState {
-  nickname: string;
-}
+const LoginPageComponent: React.FC<ISocketContext> = ({ socket }) => {
+  const [nickname, setNickname] = React.useState<string>("");
 
-class LoginPageComponent extends React.PureComponent<ISocketContext, LoginPageState> {
-  state: LoginPageState = {
-    nickname: ''
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.onKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onKeyDown);
-  }
-
-  private onKeyDown = (e: KeyboardEvent) => {
-    if (e.which === 13) { // enter
-      this.onCreateUser();
-    } else if (e.which === 8) { // backspace
-      this.setState(({nickname}) => ({
-        nickname: nickname.slice(0, -1)}
-      ));
-    } else if (/[a-z0-9\- ]/i.test(String.fromCharCode(e.which))) {
-      this.setState(({nickname}) => ({
-        nickname: nickname.length < 10 ? nickname + e.key : nickname}
-      ));
+  const onCreateUser = React.useCallback(() => {
+    if (!nickname) {
+      return;
     }
-  }
 
-  private onCreateUser = () => {
-    if (!this.state.nickname) { return; }
-    this.props.socket.emit('createUser', this.state.nickname);
-  }
+    socket.emit("createUser", nickname);
+  }, [nickname, socket]);
 
-  render() {
-    const {nickname} = this.state;
-    return (
-      <div className='login-page'>
-        <h1 className='text-glitchy-large'>Code in the Dark</h1>
-        <div className='text-glitchy-medium'>What's your name?</div>
-        <div className='nickname'>
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => (
-            <div key={i} className={'nickname-letter' + (nickname.length === i ? ' active' : '')}>
-              {nickname[i] || ''}
-            </div>
-          ))}
-        </div>
-        <button onClick={this.onCreateUser} className='button-glitchy-yellow'>
-          Enter the Dark
-        </button>
+  React.useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.which === 13) {
+        // enter
+        onCreateUser();
+      } else if (e.which === 8) {
+        // backspace
+        setNickname(nickname.slice(0, -1));
+      } else if (/[a-z0-9\- ]/i.test(String.fromCharCode(e.which))) {
+        setNickname(nickname.length < 10 ? nickname + e.key : nickname);
+      }
+    };
+    
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [onCreateUser]);
+
+  return (
+    <div className="login-page">
+      <h1 className="text-glitchy-large">Code in the Dark</h1>
+      <div className="text-glitchy-medium">What's your name?</div>
+      <div className="nickname">
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+          <div
+            key={i}
+            className={
+              "nickname-letter" + (nickname.length === i ? " active" : "")
+            }
+          >
+            {nickname[i] || ""}
+          </div>
+        ))}
       </div>
-    );
-  }
+      <button onClick={onCreateUser} className="button-glitchy-yellow">
+        Enter the Dark
+      </button>
+    </div>
+  );
 };
 
 export const LoginPage = withSocket(LoginPageComponent);
