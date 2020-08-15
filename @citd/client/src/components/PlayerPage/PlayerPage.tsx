ec61b9a923e76de85a8783e2da67ef381123f871
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2020-08-15 18:54:39 +0300

    refactor(client): move PlayerPage to FC

--- @citd/client/src/components/PlayerPage/PlayerPage.tsx
+++ @citd/client/src/components/PlayerPage/PlayerPage.tsx
@@ -10,31 +10,28 @@ import { Playground } from './Playground';
 
 type PlayerPageProps = ISocketContext & UserContext & GameContext;
 
-class PlayerPageComponent extends React.PureComponent<PlayerPageProps> {
-  componentDidMount() {
-    this.props.socket.emit('joinChannel', 'players');
-    this.props.socket.emit('joinGame', this.props.user.id);
-  }
+const PlayerPageComponent: React.FC<PlayerPageProps> = ({ socket, user, game }) => {
+  React.useEffect(() => {
+    socket.emit('joinChannel', 'players');
+    socket.emit('joinGame', user.id);
 
-  componentWillUnmount() {
-    this.props.socket.emit('leaveChannel', 'players');
-  }
+    return () => {
+      socket.emit('leaveChannel', 'players');
+    };
+  }, [socket, user.id]);
 
-  render() {
-    const { game, user } = this.props;
+  // Waiting state:
+  const isPlayerInGame = game.players.some((player) => player.id === user.id);
 
-    // Waiting state:
-    const isPlayerInGame = game.players.some((player) => player.id === user.id);
-    if (game.status === 'waiting' || !isPlayerInGame) {
-      return <PlayerLobby />;
-    }
-    // Ended state:
-    if (game.status === 'ended') {
-      return <GameOver />;
-    }
-    // Playing (and paused as overlay) state:
-    return <Playground />;
+  if (game.status === 'waiting' || !isPlayerInGame) {
+    return <PlayerLobby />;
   }
-}
+  // Ended state:
+  if (game.status === 'ended') {
+    return <GameOver />;
+  }
+  // Playing (and paused as overlay) state:
+  return <Playground />;
+};
 
 export const PlayerPage = withSocket(withUser(withGame(PlayerPageComponent)));
