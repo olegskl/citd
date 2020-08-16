f8c3c1f18372ef16588280099610cb4c0aa72056
Artem Sklyanchuk <artem.sklyanchuk@gmail.com>
2020-08-16 13:18:58 +0300

    refactor(client): move ObserverPage to FC

--- @citd/client/src/components/ObserverPage/ObserverPage.tsx
+++ @citd/client/src/components/ObserverPage/ObserverPage.tsx
@@ -12,31 +12,27 @@ import './ObserverPage.css';
 
 type ObserverPageProps = ISocketContext & GameContext;
 
-class ObserverPageComponent extends React.PureComponent<ObserverPageProps> {
-  componentDidMount() {
-    this.props.socket.emit('joinChannel', 'observers');
+const ObserverPageComponent: React.FC<ObserverPageProps> = ({ socket, game }) => {
+  React.useEffect(() => {
+    socket.emit('joinChannel', 'observers');
+
+    return () => {
+      socket.emit('leaveChannel', 'observers');
+    };
+  }, [socket]);
+
+  if (game.status === 'playing' || game.status === 'paused' || game.status === 'ended') {
+    return (
+      <div className="viewer-list">
+        <Viewer player={game.players[0]} />
+        <Preview />
+        <Viewer player={game.players[1]} />
+        <ObserverControls game={game} />
+      </div>
+    );
   }
 
-  componentWillUnmount() {
-    this.props.socket.emit('leaveChannel', 'observers');
-  }
+  return <ObserverLobby game={game} />;
+};
 
-  render() {
-    const { game } = this.props;
-
-    if (game.status === 'playing' || game.status === 'paused' || game.status === 'ended') {
-      return (
-        <div className="viewer-list">
-          <Viewer player={game.players[0]} />
-          <Preview />
-          <Viewer player={game.players[1]} />
-          <ObserverControls game={game} />
-        </div>
-      );
-    }
-
-    return <ObserverLobby game={game} />;
-  }
-}
-
-export const ObserverPage = withSocket(withGame(ObserverPageComponent));
+export const ObserverPage = withSocket(withGame(React.memo(ObserverPageComponent)));
